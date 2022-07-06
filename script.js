a045d682c8c7da2cc68a9dc9855f31b22698c7b3
JeevanMali24 <jeevanmali0398@gmail.com>
2022-07-06 11:46:00 +0530

    7-datatype

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 let js = 'amazing';
-if (js == 'amazing') alert('javascript is fun!');
+// if (js == 'amazing') alert('javascript is fun!');
 (45 + 50 - 10);
-console.log(45 + 50 - 10);  
+console.log(45 + 50 - 10);
+
+let firstName = 'jeevan';
+console.log(firstName);
+
+console.log(typeof firstName);
+console.log(typeof js);
+
+// let year;
+// console.log(year);
+
+let year = 23;
+console.log(typeof year);
+console.log(year);
+
+let javascriptisfun = true;
+console.log(javascriptisfun);
+console.log(typeof javascriptisfun)
