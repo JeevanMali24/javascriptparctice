7120ea2e1f6eb0e556057ecab4c01360b596eed0
JeevanMali24 <jeevanmali0398@gmail.com>
2022-07-08 16:41:48 +0530

    challenge

--- script.js
+++ script.js
@@ -168,7 +168,7 @@ console.log(5 + 6 + 9);                   //it does not worl in + only
 console.log('10' / '2');
 console.log('10' - '5');
 console.log('48' > '23');
-*/
+
 console.log(Boolean(0));
 console.log(Boolean(undefined));
 console.log(Boolean('jeevan'));
@@ -182,4 +182,55 @@ if (money) {
 }
 else {
     console.log("get a job");
+}
+
+const age = 18;
+if (age == 18) {
+    console.log("the age is equal ");
+}
+const fav = prompt("what's your favourite number");
+console.log(fav);
+
+if (fav === 23) {
+    console.log('cool! 23 is a amazing');
+} else if (fav === 8) {
+    console.log('cool! 8 is amazing number');
+} else {
+    console.log('this is not 23 or 7');
+}
+*/
+
+/*
+Coding Challenge #3
+There are two gymnastics teams, Dolphins and Koalas. They compete against each
+other 3 times. The winner with the highest average score wins a trophy!
+Your tasks:
+1. Calculate the average score for each team, using the test data below
+2. Compare the team's average scores to determine the winner of the competition,
+and print it to the console. Don't forget that there can be a draw, so test for that
+as well (draw means they have the same average score)
+3. Bonus 1: Include a requirement for a minimum score of 100. With this rule, a
+team only wins if it has a higher score than the other team, and the same time a
+score of at least 100 points. Hint: Use a logical operator to test for minimum
+score, as well as multiple else-if blocks 😉
+4. Bonus 2: Minimum score also applies to a draw! So a draw only happens when
+both teams have the same score and both have a score greater or equal 100
+points. Otherwise, no team wins the trophy
+Test data:
+§ Data 1: Dolphins score 96, 108 and 89. Koalas score 88, 91 and 110
+§ Data Bonus 1: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 123
+§ Data Bonus 2: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 106
+GOOD LUCK 😀
+*/
+
+const scoreDolphins = (96 + 108 + 89) / 3;
+const scoreKoalas = (88 + 91 + 110) / 3;
+console.log(scoreDolphins, scoreKoalas);
+
+if (scoreDolphins > scoreKoalas) {
+    console.log('WoW, Dolphines are winers......');
+} else if (scoreKoalas > scoreDolphins) {
+    console.log('WoW, Koalas are winers......');
+} else if (scoreDolphins === scoreKoalas) {
+    console.log('the match id draw');
 }
