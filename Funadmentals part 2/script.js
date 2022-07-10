d01a6536f03574a0c9ac0a5893fa3d69e6f28020
JeevanMali24 <jeevanmali0398@gmail.com>
2022-07-10 11:00:35 +0530

    functions

--- Funadmentals part 2/script.js
+++ Funadmentals part 2/script.js
@@ -1,10 +1,57 @@
 'use strict';
-
+/*
 let hasDriversLicense = false;
 const passTest = true;
 
 if (passTest) hasDriversLicense = true;
 if (hasDriversLicense) console.log('i can drive');
+*/
+
+//function
+
+function logger() {
+    console.log('my name is jeevan');
+}
+
+logger();
+logger();
+
+
+function fruitProcessor(apples, oranges) {
+    console.log('apples,oranges');
+    const juice = `jucice with ${apples} apple and ${oranges} oranges`;
+    return juice;
+}
+
+const drink = fruitProcessor(5, 0);
+console.log(drink);
+
+// function fruitjuice(mangoo, orange) {
+//     console.log(mangoo, orange);
+//     const juice = (`we have ${mangoo} mangoo and ${orange} orange`);
+//     return juice;
+// }
+
+// const dri = fruitjuice(5, 1);
+// console.log(dri);
+
+
+// function cars(scoda, maruti) {
+//     console.log(scoda, maruti);
+//     const car = (`there are ${scoda} scoda and ${maruti} maruti`);
+//     return car;
+// }
+// const ok = cars(4, 5);
+// console.log(ok);
+
+
+
+
+
+
+
+
+
 
 
 
