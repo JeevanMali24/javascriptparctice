61ba5e72d3762a942db4cdfdb5e69ed0ce9de506
JeevanMali24 <jeevanmali0398@gmail.com>
2022-07-10 16:19:53 +0530

    arrow

--- Funadmentals part 2/script.js
+++ Funadmentals part 2/script.js
@@ -47,6 +47,9 @@ console.log(drink);
 // const ok = cars(4, 5);
 // console.log(ok);
 
+
+
+/*
 //function decleration
 function Age1(birthyear) {
     return 2022 - birthyear;
@@ -61,3 +64,21 @@ const Age2 = function (birthyear) {
 }
 const age2 = Age2(1997);
 console.log(age1, age2);
+*/
+
+//arrow functions
+
+
+const Age3 = birthyear => 2022 - birthyear;
+const age3 = Age3(1998);
+console.log(age3);
+
+
+const yearr = (bi, fn) => {
+    const age = 2022 - bi;
+    const re = 65 - age;
+    return `${fn} retures in ${re} years`;
+}
+console.log(yearr(1998, "jee"));
+
+
