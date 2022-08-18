9470b463fdfce14987b3d76b95dda2678432ca55
JeevanMali24 <jeevanmali0398@gmail.com>
2022-08-18 20:02:33 +0530

    practice

---
+++ Funadmentals part 2/Untitled-1.js
@@ -0,0 +1,78 @@
+// 'use strict';
+
+// let hasDriversLicense = false;
+// const passTest = true;
+// if (passTest) hasDriversLicense = true;
+// if (hasDriversLicense) console.log('i can drive');
+
+
+
+// const massjohn = 58;
+// const hightjohn = 1.69;
+// const massmark = 92;
+// const hightmark = 1.90;
+
+// const BMIjohn = massjohn / hightjohn ** 2;
+// const BMImark = massmark / hightmark ** 2;
+// //const higeastBMI = BMIjohn > BMImark;
+// console.log(BMIjohn, BMImark);
+
+// if (BMIjohn > BMImark) {
+//     console.log(`John has BMI ${BMIjohn} is higest then marks BmI ${BMImark}`)
+// } else {
+//     console.log(`Mark has BMI ${BMImark}is higest then John BMI ${BMIjohn}`)
+// }
+
+
+
+//console.log(`john BMI is ${BMIjohn}, and mark BMI is ${BMImark} higest bmi is ${higtsr}`);
+
+
+
+// const inputYear = '1991';
+// console.log(Number(inputYear), inputYear);
+// console.log(Number(inputYear) + 18);
+// console.log(Number('jonas'));
+// console.log(typeof NaN);
+// console.log(String(23), (23));
+
+
+// console.log('i am' + 23 + 'years old');
+// console.log('i am' + '23' + 'years old');
+// console.log('23' + '5' + '5');            //it does not worl in + only
+// console.log(5 + 6 + 9);                   //it does not worl in + only
+// console.log('10' / '2');
+// console.log('10' - '5');
+// console.log('48' > '23');
+// console.log(Boolean(0));
+// console.log(Boolean(undefined));
+// console.log(Boolean('jeevan'));
+// console.log(Boolean(''));
+// console.log(Boolean());
+
+
+// const money = 100;
+// if (money) {
+//     console.log('dont send to much money');
+// } else {
+//     console.log('get a job');
+// }
+
+// const scoreDolpins = (5, 6, 4) / 3;
+// const scoreKoals = (5, 6, 4) / 3;
+// console.log(scoreDolpins, scoreKoals);
+
+// if (scoreDolpins > scoreKoals) {
+//     console.log(`The Dolpins are winer`);
+// } else if (scoreDolpins < scoreKoals) {
+//     console.log(`The Koals are the winer`);
+// } else if (scoreDolpins == scoreKoals) {
+//     console.log(`the maths has been tiy........`)
+// }
+
+const age = 12;
+if (age > 18) {
+    console.log(`drink a wine`);
+} else {
+    console.log(`drink milk`);
+}
--- Funadmentals part 2/script.js
+++ Funadmentals part 2/script.js
@@ -165,13 +165,142 @@ console.log(yearsuntilretirement(1970, 'kevin'));
 // }
 // console.log(fruitProcessor(2, 6));
 
-function cutfruit(fruit) {
-    return 4 * fruit;
+// function cutfruit(fruit) {
+//     return 4 * fruit;
+// }
+// function fruits(apple, orange) {
+//     const applepieces = cutfruit(apple);
+//     const orangepieces = cutfruit(orange);
+//     const juic = (`i have ${applepieces} apple and ${orangepieces} orange.`);
+//     return juic;
+//    }
+
+// console.log(fruits(2, 2));
+
+/*
+const calAge = function (birthyear) {
+    return 2037 - birthyear;
 }
-function fruits(apple, orange) {
-    const applepieces = cutfruit(apple);
-    const orangepieces = cutfruit(orange);
-    const juic = (`i have ${applepieces} apple and ${orangepieces} orange.`);
-    return juic;
+
+const yearsuntilretirement = function (birthyear, firstname) {
+    const age = calAge(birthyear);
+    const retirement = 65 - age;
+
+    if (retirement > 0) {
+        console.log(`${firstname} retires in ${retirement} years`);
+        return retirement;
+    } else {
+        console.log(`${firstname} has already retired`);
+        return -1;
+    }
 }
-console.log(fruits(2, 2));
+console.log(yearsuntilretirement(1991, 'john'));
+console.log(yearsuntilretirement(1970, 'kevin'));
+
+*/
+
+// const calage = function (birthyear, firstname) {
+//     return 2022 - birthyear;
+// }
+// const yearofretirement = function (birthyear, firstname) {
+//     const age = calage(birthyear);
+//     const retired = 65 - age;
+//     const ages = (`${firstname} retires in ${retired} year. `);
+//     return ages;
+// }
+// console.log(yearofretirement(1998, 'jeevan'));
+
+
+
+/*
+Coding Challenge #1
+Back to the two gymnastics teams, the Dolphins and the Koalas! There is a new
+gymnastics discipline, which works differently.
+Each team competes 3 times, and then the average of the 3 scores is calculated (so
+one average score per team).
+A team only wins if it has at least double the average score of the other team.
+Otherwise, no team wins!
+Your tasks:
+1. Create an arrow function 'calcAverage' to calculate the average of 3 scores
+2. Use the function to calculate the average for both teams
+3. Create a function 'checkWinner' that takes the average score of each team
+as parameters ('avgDolhins' and 'avgKoalas'), and then logs the winner
+to the console, together with the victory points, according to the rule above.
+Example: "Koalas win (30 vs. 13)"
+4. Use the 'checkWinner' function to determine the winner for both Data 1 and
+Data 2
+5. Ignore draws this time
+Test data:
+§ Data 1: Dolphins score 44, 23 and 71. Koalas score 65, 54 and 49
+§ Data 2: Dolphins score 85, 54 and 41. Koalas score 23, 34 and 27
+Hints:
+§ To calculate average of 3 values, add them all together and divide by 3
+§ To check if number A is at least double number B, check for A >= 2 * B.
+Apply this to the team's average scores 😉
+GOOD LUCK 😀
+*/
+
+// const calcAverage = (a, b, c) => (a + b + c) / 3;
+// console.log(calcAverage(3, 4, 5));
+
+// //test1
+// const scoreDolpins = calcAverage(55, 56, 20);
+// const avgKoalas = calcAverage(55, 56, 20);
+// concole.log(scoreDolpins, avgKoalas);
+
+// const checkWinner = function (avgDolpins, avgKoalas) {
+//     //console.log(avgDolpins,avgKoalas);
+//     if (avgDolpins >= avgKoalas) {
+//         console.log(`The Dolphins wins the trofy...${avgDolpins} vs. ${avgKoalas}.`);
+//     } else if (avgKoalas >= avgDolpins) {
+//         console.log(`The Koalas wines the trofy...${avgDolpins} vs ${avgKoalas}`);
+//     } else if (avgDolpins === avgKoalas) {
+//         console.log(`match drowas...${avgDolpins} vs ${avgKoalas}`);
+//     }
+// }
+// checkWinner(scoreDolpins, avgKoalas);
+// checkWinner(576, 111);
+
+
+
+
+
+// const calcAverage = (a, b, c) => (a + b + c) / 3;
+// console.log(calcAverage(3, 4, 5));
+
+// // Test 1
+// let avgDolphins = calcAverage(44, 23, 71);
+// let avgKoalas = calcAverage(65, 54, 49);
+// console.log(avgDolphins, avgKoalas);
+
+// const checkWinner = function (avgDolphins, avgKoalas) {
+//     if (avgDolphins >= 2 * avgKoalas) {
+//         console.log(`Dolphins win 🏆 (${avgDolphins} vs. ${avgKoalas})`);
+//     } else if (avgKoalas >= 2 * avgDolphins) {
+//         console.log(`Koalas win 🏆 (${avgKoalas} vs. ${avgDolphins})`);
+//     } else {
+//         console.log('No team wins...');
+//     }
+// }
+// checkWinner(avgDolphins, avgKoalas);
+
+// checkWinner(576, 111);
+
+
+// const calcAverage = (a, b, c) => (a + b + c) / 3;
+// console.log(5, 6, 7);
+
+// let avgDolphins = calcAverage(25, 45, 62);
+// let avgKoalas = calcAverage(45, 62, 23);
+// console.log(avgDolphins, avgKoalas);
+
+// const checkWinner = function (avgDolphins, avgKoalas) {
+//     if (avgDolphins >= avgKoalas) {
+//         console.log(`the Dolphines winse`);
+//     } else if (avgKoalas >= avgDolphins) {
+//         console.log(`the koalas wines`);
+//     }
+
+// }
+
+// console.log(avgDolphins, avgKoalas);
