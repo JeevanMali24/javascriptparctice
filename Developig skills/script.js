3c3f7079c49710f393ccf8f3f7b97ae0f1397508
JeevanMali24 <jeevanmali0398@gmail.com>
2022-10-10 15:33:20 +0530

    created dom

---
+++ Developig skills/script.js
@@ -0,0 +1,10 @@
+"use strict";
+const measureKelvin = function () {
+  const measureKelvin = {
+    type: "temp",
+    unit: "celsius",
+
+    value: prompt("Degrees Celsius"),
+  };
+  console.log(measureKelvin);
+};
